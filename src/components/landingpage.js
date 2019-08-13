edd8cbc47df32c41259826138463ac2754d3b8fc
Kevin Merck <kevin.merck88@gmail.com>
2019-08-13 14:59:54 -0400

    Social Links
    Working implementing social link using font awesome

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,24 +1,49 @@
-import React, { Component} from 'react'; 
-import {Grid, Cell} from 'react-mdl'
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
-class Landing extends Component { 
-render(){ 
-  return (
-<div style={{width: '100%', margin: 'auto'}}>
-  <Grid className='Landing-grid'> 
-  <Cell col={12}>
-  <img 
-  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR92jnME9n0sQlQigp-21xiervX3C9PBuQJeMxgmusXxag3Hgf3"
-  alt=""
-  className="avatar-img" 
-  />
-  <div className='banner-text'> 
-  <h1>Full Stack Web Development </h1>
-  </div>
-</Cell>
-  </Grid>
-  </div>
-)
-}}
+class Landing extends Component {
+	render() {
+		return (
+			<div style={{ width: "100%", margin: "auto" }}>
+				<Grid className="Landing-grid">
+					<Cell col={12}>
+						<img
+							src="https://previews.123rf.com/images/volmon/volmon1702/volmon170200103/72876729-cute-color-vector-illustration-of-beard-afro-black-guy-face-avatar-positive-young-black-guy-smiling-.jpg"
+							alt=""
+							className="avatar-img"
+						/>
+
+						<div className="banner-text">
+							<h1>Full Stack Web Developer </h1>
+
+							<hr />
+							
+							<p>HTML/CSS | JavaScript | React | NodeJS | Express | MongoDB </p>
+						
+							<div className="social media-links">
+							
+							{/*Linkedin*/}
+							<a href="http://google.com" rel="noopener noreferrer" target="blank">
+							<i class="fab fa-linkedin-in" arial-hidden="true"/>
+							</a>
+
+							{/*GitHub*/}
+							<a href="http://google.com" rel="noopener noreferrer" target="blank">
+							<i class="fa fa-github"  arial-hidden="true"/>
+							</a>
+
+							{/*CodePen */}
+							<a href="http://google.com" rel="noopener noreferrer" target="blank">
+							<i className= "fa fa-codepen"  arial-hidden="true"/>
+							</a>
+							
+							</div>
+						</div>
+					</Cell>
+				</Grid>
+			</div>
+		);
+	}
+}
 
 export default Landing;
